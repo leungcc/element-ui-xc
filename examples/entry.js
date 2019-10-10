fce1f20e95e68ee1f4e2d043e8c7453a098ae305
liangxuecong <497513735@qq.com>
2019-10-10 14:46:17 +0800

    更新ElementUI图标至最新

--- examples/entry.js
+++ examples/entry.js
@@ -2,23 +2,30 @@ import Vue from 'vue';
 import entry from './app';
 import VueRouter from 'vue-router';
 import Element from 'main/index.js';
-import 'packages/theme-chalk/src/index.scss';
+import hljs from 'highlight.js';
 import routes from './route.config';
-import demoBlock from './components/demo-block.vue';
-import MainFooter from './components/footer.vue';
-import MainHeader from './components/header.vue';
+import demoBlock from './components/demo-block';
+import MainFooter from './components/footer';
+import MainHeader from './components/header';
 import SideNav from './components/side-nav';
 import FooterNav from './components/footer-nav';
-import title from './i18n/title.json';
+import title from './i18n/title';
+
+import 'packages/theme-chalk/src/index.scss';
+// import './demo-styles/index.scss';
+import './assets/styles/common.css';
+import './assets/styles/fonts/style.css';
+import icon from './icon.json';
 
 Vue.use(Element);
-
 Vue.use(VueRouter);
 Vue.component('demo-block', demoBlock);
 Vue.component('main-footer', MainFooter);
 Vue.component('main-header', MainHeader);
 Vue.component('side-nav', SideNav);
 Vue.component('footer-nav', FooterNav);
+
+Vue.prototype.$icon = icon; // Icon 列表页用
 
 const router = new VueRouter({
   mode: 'hash',
@@ -27,6 +34,11 @@ const router = new VueRouter({
 });
 
 router.afterEach(route => {
+  // https://github.com/highlightjs/highlight.js/issues/909#issuecomment-131686186
+  Vue.nextTick(() => {
+    const blocks = document.querySelectorAll('pre code:not(.hljs)');
+    Array.prototype.forEach.call(blocks, hljs.highlightBlock);
+  });
   const data = title[route.meta.lang];
   for (let val in data) {
     if (new RegExp('^' + val, 'g').test(route.name)) {
@@ -35,11 +47,13 @@ router.afterEach(route => {
     }
   }
   document.title = 'Element';
+  ga('send', 'event', 'PageView', route.name);
 });
 
-console.log(routes);
+// new Vue({ // eslint-disable-line
+//   ...entry,
+//   router
 
-new Vue({ // eslint-disable-line
-  render: h => h(entry),
-  router
-}).$mount('#app');
+// }).$mount('#app');
+
+new Vue(Object.assign({}, entry, {router})).$mount('#app');
